05d4660d615e8786b165640871a6c2d580ba688e
yellowbaby1991 <13660455640@163.com>
2017-06-21 19:40:09 +0800

    实现rcview联动

--- app/src/main/java/app/yellow/github/home/explore/RepositoryListFragment.java
+++ app/src/main/java/app/yellow/github/home/explore/RepositoryListFragment.java
@@ -3,31 +3,35 @@ package app.yellow.github.home.explore;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.ImageView;
 
-import com.joanzapata.android.BaseAdapterHelper;
-import com.joanzapata.android.QuickAdapter;
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import app.yellow.github.R;
 import app.yellow.github.data.home.explore.RepositoryBean;
+import app.yellow.github.util.DisplayUtil;
+import app.yellow.github.util.SpacesItemDecoration;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class RepositoryListFragment extends Fragment {
 
-    @BindView(R.id.repositorylist_lv)
-    ListView mRepositorylistLv;
     Unbinder unbinder;
+    @BindView(R.id.repositorylist_rv)
+    RecyclerView mRepositorylistRv;
 
-    private QuickAdapter<RepositoryBean> mQuickAdapter;
-    private List<RepositoryBean> repositoryList;
+    private RepositoryListAdapter mAdapter;
+    private List<RepositoryBean> mRepositoryList;
 
     @Nullable
     @Override
@@ -40,19 +44,8 @@ public class RepositoryListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        repositoryList = new ArrayList<>();
-        mQuickAdapter = new QuickAdapter<RepositoryBean>(getActivity(), R.layout.explore_repositorylist_item, repositoryList) {
-            @Override
-            protected void convert(BaseAdapterHelper helper, RepositoryBean bean) {
-                helper.setImageUrl(R.id.avatar_url_img, bean.getOwner().getAvatar_url());
-                helper.setText(R.id.name_tv, bean.getName());
-                helper.setText(R.id.language_tv, bean.getLanguage());
-                helper.setText(R.id.description_tv, bean.getDescription());
-            }
-        };
-        mRepositorylistLv.setAdapter(mQuickAdapter);
-
+        mRepositorylistRv.addItemDecoration(new SpacesItemDecoration(DisplayUtil.px2dip(30, 1)));
+        mRepositorylistRv.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     @Override
@@ -62,7 +55,22 @@ public class RepositoryListFragment extends Fragment {
     }
 
     public void updateRepositoryList(List<RepositoryBean> repositoryList) {
-        mQuickAdapter.replaceAll(repositoryList);
-        mQuickAdapter.notifyDataSetChanged();
+        mRepositoryList = repositoryList;
+        mAdapter = new RepositoryListAdapter();
+        mRepositorylistRv.setAdapter(mAdapter);
+    }
+
+    class RepositoryListAdapter extends BaseQuickAdapter<RepositoryBean, BaseViewHolder> {
+        public RepositoryListAdapter() {
+            super(R.layout.explore_repositorylist_item, mRepositoryList);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, RepositoryBean bean) {
+            Glide.with(mContext).load(bean.getOwner().getAvatar_url()).crossFade().into((ImageView) helper.getView(R.id.avatar_url_img));
+            helper.setText(R.id.name_tv, bean.getName());
+            helper.setText(R.id.language_tv, bean.getLanguage());
+            helper.setText(R.id.description_tv, bean.getDescription());
+        }
     }
 }
